006f7fb602cda2ba73e72ab60ef8617156318d1e
Fariyal <fgousepeeran@qwinix.io>
2016-09-20 17:49:18 +0530

    files

---
+++ front-end/assignment/javascript/js.js
@@ -0,0 +1,19 @@
+
+</*function myFunction()
+{
+	document.getElementById("demo").innerHTML = "good morning";
+	newFunction()
+}
+
+function newFunction()
+{
+	document.getElementById("new").innerHTML = "good morning";
+}*/
+
+funcioon myFuctionmul(a,b)
+{
+c=a*b;
+document.write(c);
+}
+
+myFuctionmul(a,b)
