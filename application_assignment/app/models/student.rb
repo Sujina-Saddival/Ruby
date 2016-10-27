7431cd6672676336560cb8efd10597650093b14d
Fariyal <fgousepeeran@qwinix.io>
2016-10-27 17:08:24 +0530

    Assignment

---
+++ application_assignment/app/models/student.rb
@@ -0,0 +1,11 @@
+class Student < ActiveRecord::Base
+  attr_accessor :password_encrypt
+  before_save :encrypt_password
+
+def encrypt_password
+    if true
+      self.password = BCrypt::Engine.generate_salt
+      self.password = BCrypt::Engine.hash_secret(password_encrypt, password)
+    end
+  end
+end
